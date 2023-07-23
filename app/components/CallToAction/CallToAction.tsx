5c3f09eefc0a963cd9dd2fafe08006e1944d6417
Jacob Mutale <mutale1808@gmail.com>
2023-07-23 18:53:48 +0200

    First card made

--- app/components/CallToAction/CallToAction.tsx
+++ app/components/CallToAction/CallToAction.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image'
+import Image1 from '@/public/images/eurolux-lifestyle-1.png'
+
+//TODO: GRAB IMAGES AND TEXT FOR CALL TO ACTION
+
 const CallToAction = () => {
     return (
-        <div>
-            
+        <div className="mt-[20px]">
+            <div className="w-[100%] h-[389px] px-[8px]">
+                <Image
+                    alt='Card one'
+                    src={Image1}
+                    loading='lazy'
+                />
+            </div>
         </div>
     );
 }

0e8a36c0934ce3f2c69109136f0985988fa3192e
Jacob Mutale <mutale1808@gmail.com>
2023-08-13 16:33:54 +0200

    Started making Product card.

---
+++ app/components/MostPopular/MostPopular.tsx
@@ -0,0 +1,15 @@
+const MostPopular = () => {
+    //TODO: Create URL
+    return (
+        <div className="mt-[20px]">
+            <div>
+                <h2 className="text-mobile-h2 font-[600] leading-normal">
+                    Most popular this week
+                </h2>
+            </div>
+            
+        </div>
+    )
+}
+
+export default MostPopular;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CallToAction from "./components/CallToAction/CallToAction";
 import CategoryGrid from "./components/Catergory/CategoryGrid";
 import Hero from "./components/Hero/Hero";
+import MostPopular from "./components/MostPopular/MostPopular";
 import Promotions from "./components/Promotions/Promotions";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <CallToAction />
       <CategoryGrid/>
       <Promotions/>
+      <MostPopular/>
     </main>
   )
 }
